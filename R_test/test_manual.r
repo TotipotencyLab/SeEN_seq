f146f52385221b38395e4ca289695b3aaa584af1
Siwat Chad Ruangroengkulrith <siwat.rua@gmail.com>
2025-05-14 20:27:19 +0200

    Add R script to turn sample table to input for QuasR mapping

--- R_test/test_manual.r
+++ R_test/test_manual.r
@@ -1,3 +1,15 @@
+library(yaml)
+library(stringr)
+library(dplyr)
+library(tibble)
+library(tidyr)
+# library(GenomicRanges)
+# library(GenomicFeatures)
+# library(rtracklayer)
+library(ShortRead)
+library(Rsamtools)
+library(QuasR)
+
 # Set up variables for testing
 wd <- "/fs/pool/pool-toti-bioinfo/bioinfo/siwat_chad/dev/lab_pipelines/SeEN_seq"
 setwd(wd)
@@ -12,3 +24,8 @@ sample_df_path <- config$sample_table_path
 sample_df <- read.table(sample_df_path, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 sample_df <- find_fastq(sample_df, config$fastq_dir)
 sample_df <- sample_df_add_lib_layout(sample_df)
+
+#region make QuasR input
+source(paste0(wd, "/scripts/make_QuasR_input.r"))
+QuasR_df_list <- sample_df_to_QuasR_table(sample_df, output_prefix = "./results/QuasR/QuasR_sample", Return = TRUE, verbose = TRUE)
+
---
+++ scripts/make_QuasR_input.r
@@ -0,0 +1,61 @@
+# Making input table for QuasR
+sample_df_to_QuasR_table <- function(sample_df, output_prefix="./QuasR_sample", Return=FALSE, verbose=FALSE) {
+  # Dependencies:
+  #  - find_fastq.r: sample_df_add_lib_layout function
+  
+  # Corner cases:
+  # 1) sample_df has mixed of paired and single-end library layout
+  # 2) sample_df has samples without fastq files
+  
+  # Check library layout
+  # This should add new column: `lib_layout` annotating whether each sample is single-end ("single") or paired-end ("paired")
+  # Sample without fastq files will be NA
+  sample_df <- sample_df_add_lib_layout(sample_df)
+
+  # lib_layout column is in a way
+  if (any(is.na(sample_df$lib_layout))) {
+    tmp_df <- sample_df[is.na(sample_df$lib_layout), , drop = FALSE]
+    sample_df <- sample_df[!is.na(sample_df$lib_layout), , drop = FALSE]
+    warning(
+      "The input contains samples without fastq files. These will be ignored",
+      paste0("Samples without fastq files:\n", paste0(tmp_df$sample[is.na(tmp_df$lib_layout)], collapse = "\n"))
+    )
+  }
+  
+  # split single-end and paired-end samples
+  sample_SE_df <- sample_df[sample_df$lib_layout == "single", ]
+  sample_PE_df <- sample_df[sample_df$lib_layout == "paired", ]
+  if (nrow(sample_SE_df) > 0 && nrow(sample_PE_df) > 0) {
+    message("This project contains both samples with single-end and paired-end fastq files. Making separated QuasR input tables for each library layout.")
+  }
+  
+  if (nrow(sample_SE_df) > 0 || nrow(sample_PE_df) > 0) {
+    out_dir <- dirname(output_prefix)
+    if(!dir.exists(out_dir)) {
+      dir.create(out_dir, recursive = TRUE)
+    }
+  }
+
+  # Make the input table for QuasR
+  if(nrow(sample_SE_df) > 0){
+    # Input table for SE required two column: FileName and SampleName
+    sample_SE_df <- sample_SE_df %>%
+      select(FileName = fastq_SE, SampleName = sample)
+    out_SE_file <- paste0(output_prefix, "_SE.txt")
+    if(verbose) message("Writing QuasR input table for single-end samples to ", out_SE_file)
+    write.table(sample_SE_df, file = out_SE_file, sep = "\t", row.names = FALSE, quote = FALSE)
+  }
+
+  if(nrow(sample_PE_df) > 0){
+    # Input table for PE required three column: FileName1, FileName2 and SampleName
+    sample_PE_df <- sample_PE_df %>%
+      select(FileName1 = fastq_R1, FileName2 = fastq_R2, SampleName = sample)
+    out_PE_file <- paste0(output_prefix, "_PE.txt")
+    if(verbose) message("Writing QuasR input table for paired-end samples to ", out_PE_file)
+    write.table(sample_PE_df, file = out_PE_file, sep = "\t", row.names = FALSE, quote = FALSE)
+  }
+
+  if (Return) {
+    return(list(SE = sample_SE_df, PE = sample_PE_df))
+  }
+}
