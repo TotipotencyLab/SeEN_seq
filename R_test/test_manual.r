4ecf4acfdaf87c5fba880bf3725fe352e631c443
Siwat Chad Ruangroengkulrith <siwat.rua@gmail.com>
2025-05-14 19:28:04 +0200

    Add R script for fastq searching

---
+++ R_test/test_manual.r
@@ -0,0 +1,12 @@
+# Set up variables for testing
+wd <- "/fs/pool/pool-toti-bioinfo/bioinfo/siwat_chad/dev/lab_pipelines/SeEN_seq"
+setwd(wd)
+config <- yaml::read_yaml("config.yaml")
+str(config)
+
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+#region Test fastq search
+# /////////////////////////////////////////////////////////////////////////////////////////////////
+sample_df_path <- config$sample_table_path
+sample_df <- read.table(sample_df_path, header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+sample_df <- find_fastq(sample_df, config$fastq_dir)
